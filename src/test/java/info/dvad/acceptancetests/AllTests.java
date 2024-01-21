cc8b190d100bf98f76112cc28474a11a0731e170
svr94 <viktoria.romanovna@gmail.com>
2024-01-22 00:53:51 +0100

    v1

---
+++ src/test/java/info/dvad/acceptancetests/AllTests.java
@@ -0,0 +1,16 @@
+package info.dvad.acceptancetests;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = {"info.dvad"},
+        features = ".",
+        format = {"pretty", "json:target/cucumber-reports/cucumber.json"},
+        tags = {"@complete"}
+)
+public class AllTests {
+}
